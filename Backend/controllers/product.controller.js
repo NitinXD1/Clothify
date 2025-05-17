d28c4bcc681b36851f36485ee9a5a17d365cc5d1
NitinXD1 <nitinkachera98@gmail.com>
2025-05-17 17:43:40 +0530

    Add product management features: implement product creation, deletion, and caching; integrate Cloudinary for image uploads; update routes for new functionalities.

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -1,5 +1,18 @@
 import { redis } from "../lib/redis.js";
 import { Product } from "../models/product.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+const updateFeaturedProductCache = async () =>{
+    try {
+        
+        const featuredProducts = await Product.find({isFeatured : true}).lean();
+
+        await redis.set("featuredProducts", JSON.stringify(featuredProducts));
+
+    } catch (error) {
+        console.log("Error in updating redis cache function")
+    }
+}
 
 export const getAllProducts = async (req,res) => {
     try {
@@ -19,15 +32,15 @@ export const getFeaturedProducts = async(req,res) =>{
         let featuredProducts = await redis.get("featured_products")
 
         if(featuredProducts){
-            return res.status(201).json(JSON.parse(featuredProducts))
+            return res.status(202).json(JSON.parse(featuredProducts))
         }
-        
+
         //.lean is helpful in getting the data in plain JS objects instead of mongodb objects
         //performance improves
         featuredProducts = await Product.find({isFeatured:true}).lean()
 
         if(!featuredProducts){
-            return res.status(404).json({message : "No featured products available"})
+            return res.status(403).json({message : "No featured products available"})
         }
 
         await redis.set("featured_products",JSON.stringify(featuredProducts))
@@ -37,5 +50,151 @@ export const getFeaturedProducts = async(req,res) =>{
     } catch (error) {
         console.log("Error in getFeaturedProducts controller",error.message)
         return res.status(500).json({message : "Server error",error : error.message})
+    }
+}
+
+export const createProduct = async(req,res) => {
+    try {
+        const { name , price , description ,category , image} = req.body
+
+        if ([name, price, description, category].some(field => typeof field === 'string' ? field.trim() === "" : field === undefined || field === null)) {
+            return res.status(400).json({ message: "All fields are necessary and mandatory" });
+        }
+
+        let cloudinaryResponse = null
+
+        if(image){
+            cloudinaryResponse = await cloudinary.uploader.upload(image,{folder : "products"})
+        }
+
+        const product = await Product.create(
+            {
+                name,
+                price,
+                image : cloudinaryResponse?.secure_url ? cloudinaryResponse?.secure_url : "",
+                description,
+                category,
+            }
+        )
+
+        return res.status(201).json(
+            {
+                message : "Product created successfully",
+                product
+            }
+        )
+
+    } catch (error) {
+        console.log("Error in product creation controller",error.message)
+        return res.status(500).json({message : "Server Error",error : error.message})
+    }
+}
+
+export const deleteProduct = async(req,res) => {
+    try {
+        const id = req.params.id
+    
+        const product = await Product.findById(id)
+
+        if(!product){
+            return res.status(404).json({message : "Product not found"})
+        }
+
+        if(product.image){
+            const publicId = product.image.split("/").pop().split(".")[0]
+            try {
+                await cloudinary.uploader.destroy(`products/${publicId}`)
+                console.log("Cloudinary image deleted")
+            } catch (error) {
+                console.log("Error in deleting the cloudinary image")
+            }
+        }
+
+        await Product.findByIdAndDelete(req.params.id)
+    
+        return res.status(200).json({message : "Product deleted successfully"})
+    } catch (error) {
+        console.log("Error in delete product controller")
+        return res.status(500).json({message:"Internal Server Error",error : error.message})
+    }
+}
+
+export const getRecommendedProducts = async(req,res) => {
+    try {
+        const products = await Product.aggregate([
+            {
+                $sample : {
+                    size : 3
+                }
+            },
+            {
+                $project : {
+                    _id : 1,
+                    name : 1,
+                    price : 1,
+                    image : 1,
+                    description : 1,
+                }
+            }
+        ])
+    
+        res.status(200).json(products)
+    } catch (error) {
+        console.log("error in the get recommended products controller")
+        return res.status(500).json(
+            {
+                message : "Server side Error",
+                error : error.message
+            }
+        )
+    }
+}
+
+export const getProductByCategory = async(req,res) => {
+    try {
+        const {category} = req.params
+    
+        const products = await Product.find({category})
+    
+        return res.status(200).json(products)
+    } catch (error) {
+        console.log("Error in getting Products by category controller")
+        return res.status(500).json({message : "Internal Server Error",error : error.message})
+    }
+}
+
+export const toggleFeaturedProduct = async (req,res) => {
+    try {
+
+        const id = req.params.id
+
+        const product = await Product.findById(id)
+
+        if(!product){
+            return res.status(404).json({
+                message : "Product doesnt exists"
+            })
+        }
+
+        product.isFeatured = !product.isFeatured
+
+        await product.save()
+
+        await updateFeaturedProductCache()
+
+        return res.status(200).json(
+            {
+                message : "Featured updated successfully"
+            }
+        )
+
+    } catch (error) {
+        
+        console.log("Error in toggle Featured controller",error.message)
+        return res.status(500).json({
+            message : "Internal Server Error",
+            error : error.message
+        })
+
     }
 }
--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -1,10 +1,24 @@
 import {Router} from 'express'
-import { getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js'
+import { 
+    getAllProducts,
+    getFeaturedProducts ,
+    createProduct ,
+    deleteProduct ,
+    getRecommendedProducts,
+    getProductByCategory,
+    toggleFeaturedProduct
+} from '../controllers/product.controller.js'
 import { protectRoute , adminRoute } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
 router.route("/").get(protectRoute , adminRoute , getAllProducts)
 router.route("/featured").get(getFeaturedProducts)
+router.route("/recommendations").get(protectRoute , getRecommendedProducts)
+router.route("/category/:category").get(getProductByCategory)
+router.route("/createProduct").get(protectRoute , adminRoute , createProduct)
+router.route("/:id").delete(protectRoute , adminRoute , deleteProduct)
+router.route("/:id").patch(protectRoute , adminRoute , toggleFeaturedProduct)
+
 
 export default router
